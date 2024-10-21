2ea9b1437b568d0849bcdf30e955174eec11ad63
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-10-22 00:16:27 +0300

    fix: add quick solution for the runtime not found v2.0

--- scripts/setup_native_deps.sh
+++ scripts/setup_native_deps.sh
@@ -87,30 +87,11 @@ install_rust() {
 	echo "Rust installed successfully."
 }
 
-install_cairo_native_runtime() {
-  install_rust || { echo "Error: Failed to install Rust"; exit 1; }
-
-	git clone https://github.com/lambdaclass/cairo_native.git
-	pushd ./cairo_native || exit 1
-	cargo build -p cairo-native-runtime --release --all-features --quiet
-	popd || exit 1
-
-	cp ./cairo_native/target/release/libcairo_native_runtime.a $(pwd)/libcairo_native_runtime.a
-	rm -rf ./cairo_native
-	chmod 644 $(pwd)/libcairo_native_runtime.a
-
-	export CAIRO_NATIVE_RUNTIME_LIBRARY="$(pwd)/libcairo_native_runtime.a"
-
-  echo "CAIRO_NATIVE_RUNTIME_LIBRARY=$CAIRO_NATIVE_RUNTIME_LIBRARY"
-
-	[ -n "$GITHUB_ACTIONS" ] && echo "CAIRO_NATIVE_RUNTIME_LIBRARY=$CAIRO_NATIVE_RUNTIME_LIBRARY" >> $GITHUB_ENV
-}
-
 main() {
   [ "$(uname)" = "Linux" ] && install_essential_deps_linux
 
 	setup_llvm_deps
-	install_cairo_native_runtime
+  install_rust
 
 	echo "LLVM and Cairo native runtime dependencies installed successfully."
 }
